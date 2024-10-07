a9eb6208f3aaa229bc65f6f3200b3b9205465a4f
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-07 13:44:07 +0200

    Add month to console
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const Input = require("prompt-input");
 
 const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth();
 
 export const input = new Input({
   message: "Vilket år föddes du? format: yyyy-mm-dd",
@@ -14,15 +15,18 @@ input.ask(function (answers: string) {
 export function calculateAge(number: string) {
   const year = number.split("-")[0];
   const month = number.split("-")[1];
-  const currentMonth = new Date().getMonth();
-  let age: number;
+  let yourAge = 0;
 
-  if (currentMonth > parseInt(month))
-    return (age = currentYear - parseInt(year));
-  if (currentMonth < parseInt(month))
-    return (age = currentYear - parseInt(year) - 1);
+   if (parseInt(month) < 0 || parseInt(month) > 12)
+     throw new Error("Ogiltigt manad.");
+   const test = currentMonth - parseInt(month);
 
-  console.log(currentMonth);
-  console.log(month);
-  console.log(year);
-}
+  if (currentMonth > parseInt(month)){
+   yourAge = currentYear - parseInt(year);
+   return `You are ${yourAge} years and ${test} months old. `;
+  }
+
+  if (currentMonth < parseInt(month)){
+    yourAge = currentYear - parseInt(year) - 1;
+    return (`You are ${yourAge} years and ${test} months old. `)
+  }}
