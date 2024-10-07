c14406624bac055f8a85ff6dcc31453d9c91206b
defycab <love.bellinder@gmail.com>
2024-10-07 13:12:47 +0200

    month test passed.
    Co-authored-by: Adrienn <apozsgai97@gmail.com>
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,7 +6,7 @@ describe("calculateAge", () => {
   test("should return age", () => {
     assert.equal(calculateAge("1999-03-12"), 25);
   });
-  test("should return month", () => {
-    assert.equal(calculateAge("1999-03-12"), 25);
+  test("should return 24 if birthmonth is november", () => {
+    assert.equal(calculateAge("1999-11-12"), 24);
   });
 });
