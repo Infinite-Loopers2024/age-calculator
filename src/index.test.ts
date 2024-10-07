65cd3a9f235d7964bd90208ca08fe8e6fca0e773
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-07 11:41:56 +0200

    ask for month Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se> Co-authored-by: Love <love.bellinder@gmail.com> Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
 import { describe, test } from "node:test";
-import { calculateAge } from  "./index";
+import { calculateAge } from "./index";
 import assert from "node:assert";
 
 describe("input year", () => {
   test("should return age", () => {
     const currentYear = new Date().getFullYear();
 
-    assert.equal(calculateAge(1999), currentYear-1999);
+    assert.equal(calculateAge("1999-03-12"), currentYear - 1999);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ const Input = require("prompt-input");
 const currentYear = new Date().getFullYear();
 
 export const input = new Input({
-  message: "Vilket år föddes du?",
-  validate: (answers: number) =>
-    answers > currentYear ? "Skriv in giltigt år" : true,
+  message: "Vilket år föddes du? format: yyyy-mm-dd",
 });
 
-input.ask(function (answers: number) {
+input.ask(function (answers: string) {
   const age = calculateAge(answers);
   console.log(age);
 });
 
-export function calculateAge(number: number) {
-  const age = currentYear - number;
+export function calculateAge(number: string) {
+  const year = number.split("-")[0];
+  const month = number.split("-")[1];
+  console.log(month);
+  console.log(year);
+  const age = currentYear - parseInt(year);
   return age;
 }
