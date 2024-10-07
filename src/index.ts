5a1f92e4e5a1d5ffce4a6a410cdac26790a3e529
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-07 10:28:16 +0200

    Can take input
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hej');
+const Input = require ("prompt-input");
+
+
+const input = new Input({ 
+ namn: "först", 
+ message: "Vad heter du?"
+})
+
+
+input.ask( function (answers: any) {
+ console.log(answers);
+})
